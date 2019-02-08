6117bec020b66de1d7edf6b1458fbaa456b38612
Sunitha Priyadarshini Sampath <sunithap@bu.edu>
2019-02-08 11:49:22 -0500

    read images using tesseract

---
+++ readCard.py
@@ -0,0 +1,21 @@
+import argparse
+import cv2
+import pytesseract as pt
+
+def detectText(image):
+	"""Read text from image using tesseract"""
+	#print(image)
+	img = cv2.imread(image)
+	img = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+	gray = cv2.threshold(img, 0, 255,cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
+	# cv2.imshow("image",gray)
+	# cv2.waitKey(0)
+	result = pt.image_to_string(gray, lang="eng")
+	print(result)
+
+
+if __name__ == "__main__":
+	a = argparse.ArgumentParser()
+	a.add_argument("--image", help= "path to image")
+	args = a.parse_args()
+	detectText(args.image)
