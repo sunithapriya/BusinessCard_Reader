da7b62dfcf39ae0be09ab342614bb2142eb6a808
ArvindK <karthick_aravindan@ubuntu.ubuntu-domain>
2019-02-12 00:16:35 +0530

    Error in Laplacian

---
+++ package.py
@@ -0,0 +1,6 @@
+import argparse
+import cv2
+import pytesseract as pt
+import linkedinUrl
+import numpy as np
+import pandas as pd
--- readCard.py
+++ readCard.py
@@ -1,18 +1,27 @@
-import argparse
-import cv2
-import pytesseract as pt
-import linkedinUrl
+from package import *
 
 def detectText(image):
 	"""Read text from image using tesseract"""
 	img = cv2.imread(image)
-	img = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-	gray = cv2.threshold(img, 0, 255,cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
-	# cv2.imshow("image",gray)
-	# cv2.waitKey(0)
-	result = pt.image_to_string(gray, lang="eng")
+
+	kernel = np.ones((5,5),np.float32)/25
+	img = cv2.filter2D(img,-1,kernel)
+	laplacian = cv2.Laplacian(img,cv2.CV_64F)
+	
+
+	
+
+	# img = cv2.cvtColor(laplacian,cv2.COLOR_BGR2GRAY)
+	# gray = cv2.threshold(img, 0, 255,cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
+	
+	cv2.imshow('image',laplacian)
+	cv2.waitKey(0)
+	cv2.destroyAllWindows()
+	
+
+	result = pt.image_to_string(laplacian, lang="eng")
 	print(result)
-	url_list=linkedinUrl.get_link(result)
+	# url_list=linkedinUrl.get_link(result)
 
 
 if __name__ == "__main__":
